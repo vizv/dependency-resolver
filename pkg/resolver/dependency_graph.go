1138904ad9068d43d21c568cbab0bca3f96524a0
Wenxuan Zhao <viz@linux.com>
2021-12-24 14:49:21 +0800

    WIP

--- pkg/resolver/dependency_graph.go
+++ pkg/resolver/dependency_graph.go
@@ -11,57 +11,57 @@ type DependencyGraph struct {
 
 // getOrCreateNode returns a node by name,
 // the node gets created if not exist in graph
-func (r *DependencyGraph) getOrCreateNode(name string) *DependencyNode {
-	np, exists := r.nodes[name]
+func (g *DependencyGraph) getOrCreateNode(name string) *DependencyNode {
+	n, exists := g.nodes[name]
 
 	if !exists {
-		np = NewNode(name)
-		r.nodes[name] = np
-		r.allNodes.Add(np)
+		n = NewNode(name)
+		g.nodes[name] = n
+		g.allNodes.Add(n)
 	}
 
-	return np
+	return n
 }
 
 // getOrCreateNodes returns dependant node and prerequisite node for a given dependency,
 // these nodes get created if not exist in graph
-func (r *DependencyGraph) getOrCreateNodes(dependency *Dependency) (*DependencyNode, *DependencyNode) {
-	dependant := r.getOrCreateNode(dependency.Dependant)
-	prerequisite := r.getOrCreateNode(dependency.Prerequisite)
+func (g *DependencyGraph) getOrCreateNodes(d *Dependency) (*DependencyNode, *DependencyNode) {
+	dependant := g.getOrCreateNode(d.Dependant)
+	prerequisite := g.getOrCreateNode(d.Prerequisite)
 
 	return dependant, prerequisite
 }
 
 // addToLookupMap adds a dependant with a prerequisite to a map for reverse lookup
-func (r *DependencyGraph) addToLookupMap(dependant *DependencyNode, prerequisite *DependencyNode) {
-	sp, exists := r.lookupMap[prerequisite]
+func (g *DependencyGraph) addToLookupMap(dep *DependencyNode, pre *DependencyNode) {
+	s, exists := g.lookupMap[pre]
 	if !exists {
-		sp = NewSet()
-		r.lookupMap[prerequisite] = sp
+		s = NewSet()
+		g.lookupMap[pre] = s
 	}
-	sp.Add(dependant)
+	s.Add(dep)
 }
 
 // addDependency add a dependency to a graph,
 // and update information stored in the graph
-func (r *DependencyGraph) addDependency(dependency *Dependency) {
-	dependant, prerequisite := r.getOrCreateNodes(dependency)
-	dependant.Prerequisites.Add(prerequisite)
+func (g *DependencyGraph) addDependency(d *Dependency) {
+	dep, pre := g.getOrCreateNodes(d)
+	dep.Prerequisites.Add(pre)
 
-	r.dependantNodes.Add(dependant)
-	r.prerequisiteNodes.Add(prerequisite)
+	g.dependantNodes.Add(dep)
+	g.prerequisiteNodes.Add(pre)
 
-	r.addToLookupMap(dependant, prerequisite)
+	g.addToLookupMap(dep, pre)
 }
 
 // leaves calculates nodes without prerequisite
-func (r *DependencyGraph) leaves() *DependencyNodeSet {
-	return r.prerequisiteNodes.Difference(r.dependantNodes)
+func (g *DependencyGraph) leaves() *DependencyNodeSet {
+	return g.prerequisiteNodes.Difference(g.dependantNodes)
 }
 
-func (r *DependencyGraph) resolveAll(nodeSet *DependencyNodeSet, sequence uint) bool {
-	for leaf := range nodeSet.Iter() {
-		if !r.resolve(leaf, sequence) {
+func (g *DependencyGraph) resolveAll(s *DependencyNodeSet, seq uint) bool {
+	for leaf := range s.Iter() {
+		if !g.resolve(leaf, seq) {
 			return false
 		}
 	}
@@ -71,22 +71,20 @@ func (r *DependencyGraph) resolveAll(nodeSet *DependencyNodeSet, sequence uint) 
 
 // resolve the dependency graph recursively,
 // update this node and all dependants only if current sequence is larger
-func (r *DependencyGraph) resolve(node *DependencyNode, sequence uint) bool {
-	if node.visited {
+func (g *DependencyGraph) resolve(n *DependencyNode, seq uint) bool {
+	if n.visited {
 		return false
 	}
-	node.visited = true
-	defer func() { node.visited = false }()
+	n.visited = true
+	defer func() { n.visited = false }()
 
-	if sequence <= node.Sequence {
+	if seq <= n.Sequence {
 		return true
 	}
-	node.Sequence = sequence
-	if parents, okay := r.lookupMap[node]; okay {
-		for leaf := range parents.Iter() {
-			if !r.resolve(leaf, sequence+1) {
-				return false
-			}
+	n.Sequence = seq
+	if pres, okay := g.lookupMap[n]; okay {
+		if !g.resolveAll(pres, seq+1) {
+			return false
 		}
 	}
 
